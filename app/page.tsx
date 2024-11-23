a3f715b791b8034a24dbed81feeb11f6826afb35
Rashida Strozier <rashida.strozier@gmail.com>
2024-11-23 17:24:36 -0500

    build 100% and no errors great stopping or pull down point

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 "use client";
-
 import { useUserData } from "./universal/hooks/useUserdata";
 import LoginToSpotifyBtn from "./components/buttons/LoginToSpotifyBtn";
 import logo from "../public/imgs/final-logo-pulse-playlist.png"
 import Image from 'next/image';
 
 export default function Page() {
-  const { loading } = useUserData();
+    const { loading } = useUserData();
 
-  return loading ? (
-    <div className="flex items-center justify-center h-screen">
-      <p>Loading...</p>
-    </div>
-  ) : (
-    <>
-      <Image src={logo} alt="Pulse Playlist Logo" className="mb-4" width={300} height={100} /> {/* Set width and height */}
-          <LoginToSpotifyBtn />
-    </>
-  );
+    return loading ? (
+        <div className="flex items-center justify-center h-screen">
+            <p>Loading...</p>
+        </div>
+    ) : (
+        <>
+            <div className="flex flex-col items-center p-1 justify-center min-h-screen">
+                <Image src={logo} alt="Pulse Playlist Logo" className="mb-4" width={300} height={100} /> 
+                <LoginToSpotifyBtn />
+            </div>
+        </>
+    );
 }
