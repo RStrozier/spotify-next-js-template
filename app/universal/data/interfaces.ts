c8536e53ae962999a743fd6d51c4c52413eeaddf
Rashida Strozier <rashida.strozier@gmail.com>
2024-11-23 16:24:20 -0500

    added data sets and interfaces

---
+++ app/universal/data/interfaces.ts
@@ -0,0 +1,6 @@
+// mood context
+export type MoodKey = 'Happy' | 'Sad' | 'Energetic' | 'Relaxed' | 'Angry' | 'Anxious' | 'Bored' |
+    'Confident' | 'Disappointed' | 'Excited' | 'Frustrated' | 'Grateful' |
+    'Hopeful' | 'Inspired' | 'Lonely' | 'Nostalgic' | 'Overwhelmed' |
+    'Proud' | 'Relieved' | 'Surprised' | 'Tired' | 'Worried' |
+    'Curious' | 'Content' | 'Playful' | 'Determined';
